4415603c090dfe0c03286312a0d9f6e9d3de2342
YuSeokHyun <fkstndnjs@naver.com>
2022-10-20 11:42:08 +0900

    feat: controller service basic test

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  //http://localhost:8000/cats/hello
+  @Get('hello/:id/:idd')
+  getHello(
+    @Req() req: Request,
+    @Body() body,
+    @Param('id') id: string,
+    @Param('idd') idd: string,
+  ): string {
+    console.log(req);
+    console.log(id, idd);
+
+    // return this.appService.getHello();
+    return 'Hello';
   }
 }
