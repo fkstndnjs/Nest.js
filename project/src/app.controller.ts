1ca9c6214c84cae92996b0793f05b809cba94a05
YuSeokHyun <fkstndnjs@naver.com>
2022-12-13 09:06:45 +0900

    fix

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,23 +1,16 @@
-import { Body, Controller, Get, Param, Query, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from 'src/cats/cats.service';
 import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
 
-  //http://localhost:8000/cats/hello
-  @Get('hello/:id/:idd')
-  getHello(
-    @Req() req: Request,
-    @Body() body,
-    @Param('id') id: string,
-    @Param('idd') idd: string,
-  ): string {
-    console.log(req);
-    console.log(id, idd);
-
-    // return this.appService.getHello();
-    return 'Hello';
+  @Get()
+  getTest() {
+    return this.catsService.getHello();
   }
 }
