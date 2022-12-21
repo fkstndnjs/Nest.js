04a1cbca3b3391134b3dc8dc2266d908987f18d3
YuSeokHyun <fkstndnjs@naver.com>
2022-12-21 09:47:48 +0900

    fix

--- first-project/src/app.controller.ts
+++ first-project/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('animal')
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get('cat')
