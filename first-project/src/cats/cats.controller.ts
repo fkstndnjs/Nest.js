7d482ee3de88b41989dfae40f9e30cf11db4c367
fkstndnjs <fkstndnjs@naver.com>
2022-12-21 22:21:32 +0900

    fix

--- first-project/src/cats/cats.controller.ts
+++ first-project/src/cats/cats.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { CatsService } from 'src/cats/cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+}
