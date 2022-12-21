1b097916062d5889302d9a770d8bc4bce08c3a85
YuSeokHyun <fkstndnjs@naver.com>
2022-12-21 10:09:31 +0900

    fix

--- first-project/src/app.controller.ts
+++ first-project/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
