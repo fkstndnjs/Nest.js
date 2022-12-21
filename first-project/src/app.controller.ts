d928e8e36e847cda13d7ee6a59991b35f16cf57f
YuSeokHyun <fkstndnjs@naver.com>
2022-12-21 10:09:23 +0900

    fix

--- first-project/src/app.controller.ts
+++ first-project/src/app.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
     return 'hello';
   }
 }
