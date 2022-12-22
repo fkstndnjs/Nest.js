31ca5e4dd18f64e881bbc02e7c4ac34d62b1ecbb
fkstndnjs <fkstndnjs@naver.com>
2022-12-22 20:54:52 +0900

    fix

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { CatsService } from './cats/cats.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
