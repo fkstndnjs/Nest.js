2f3a3ece87943c5e2791d3e6e4363c75104f63c2
fkstndnjs <fkstndnjs@naver.com>
2022-12-22 21:22:53 +0900

    fix

--- first-project/src/logger/logger.middleware.ts
+++ first-project/src/logger/logger.middleware.ts
@@ -1,8 +1,9 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: any, next: () => void) {
     next();
   }
 }
