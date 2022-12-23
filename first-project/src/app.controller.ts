fdf0214c00a5440c27f84f1cf474de39090ae141
YuSeokHyun <fkstndnjs@naver.com>
2022-12-23 16:07:19 +0900

    fix

--- first-project/src/app.controller.ts
+++ first-project/src/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Query,
+  UseFilters,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CatsService } from './cats/cats.service';
+import { HttpExceptionFilter } from './http-exception/http-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getHello(): string {
+    throw new HttpException('HTTP EXCEPTION', 500);
+
     return this.appService.getHello();
   }
 }
