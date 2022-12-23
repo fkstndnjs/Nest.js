336eb7153eb75a495dbabd92cd6fe6891200b6ee
YuSeokHyun <fkstndnjs@naver.com>
2022-12-23 17:17:57 +0900

    fix

--- first-project/src/app.controller.ts
+++ first-project/src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getHello(): string {
     throw new HttpException('HTTP EXCEPTION', 500);
 
