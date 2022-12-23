66aec8264fbb3f73d690de1bc9a09aaaaa30e538
YuSeokHyun <fkstndnjs@naver.com>
2022-12-23 15:53:09 +0900

    fix

--- first-project/src/app.controller.ts
+++ first-project/src/app.controller.ts
@@ -4,10 +4,7 @@ import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly catsService: CatsService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
