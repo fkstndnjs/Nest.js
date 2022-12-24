f8a30863d67af23a2fc85b66ec669e5873674bbd
fkstndnjs <fkstndnjs@naver.com>
2022-12-24 14:50:22 +0900

    fix

--- first-project/src/parse-string/parse-string.pipe.ts
+++ first-project/src/parse-string/parse-string.pipe.ts
@@ -3,6 +3,7 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class ParseStringPipe implements PipeTransform {
   transform(value: number, metadata: ArgumentMetadata) {
+    console.log(value);
     return `${value}`;
   }
 }
