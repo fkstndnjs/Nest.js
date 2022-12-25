512fec27f9c0665be59b040eb2292852a6f041a9
fkstndnjs <fkstndnjs@naver.com>
2022-12-25 16:38:53 +0900

    fix

--- first-project/src/success/success.interceptor.ts
+++ first-project/src/success/success.interceptor.ts
@@ -1,9 +1,20 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class SuccessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    console.log('Before...');
+
+    return next.handle().pipe(
+      tap(() => {
+        console.log('After...');
+      }),
+    );
   }
 }
