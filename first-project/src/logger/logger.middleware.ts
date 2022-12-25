34f073a5ab5ccc74f1d802fb5c1967311bdbcceb
fkstndnjs <fkstndnjs@naver.com>
2022-12-25 16:04:08 +0900

    fix

--- first-project/src/logger/logger.middleware.ts
+++ first-project/src/logger/logger.middleware.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    console.log('req.path', req.path);
     console.log('middleware Test');
 
     next();
